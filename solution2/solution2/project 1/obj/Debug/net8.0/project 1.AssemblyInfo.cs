3ad59dfe37526c6e05a7a9352f12059e2eb80383
Sheika <sheika02x2@gmail.com>
2026-04-19 22:03:17 +0400

    Task 8 – Prime Number Checker in a Range

--- solution2/solution2/project 1/obj/Debug/net8.0/project 1.AssemblyInfo.cs
+++ solution2/solution2/project 1/obj/Debug/net8.0/project 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65d5536f6b976deedc179a8bf0fd4520acfc89b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c9fb552f0a3fd45755dd774ba3244e47e3ba1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
