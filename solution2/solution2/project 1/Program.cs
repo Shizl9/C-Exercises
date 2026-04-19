bf1cee165dea82a3f8594732089c886cb4190077
Sheika <sheika02x2@gmail.com>
2026-04-19 19:32:22 +0400

    first commit

---
+++ solution2/solution2/project 1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 – Day Name Printer
+            Console.WriteLine("Enter number from(1-7):");
+            int day = int.Parse(Console.ReadLine());
+            switch (day) { 
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");   
+                break;
+            case 3:
+                Console.WriteLine("Tuseday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday"); 
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+
+            }
+
+            //Task 2 – Multiplication Tabl
+
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(number + " x " + i + " = " + (number * i));
+
+            //}
+
+        }
+    }
+}
