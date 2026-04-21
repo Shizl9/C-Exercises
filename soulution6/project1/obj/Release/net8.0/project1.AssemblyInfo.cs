ef8c726ab74e0d2d647130a6a44e180c5523cc99
Sheika <sheika02x2@gmail.com>
2026-04-21 14:44:16 +0400

    add students names

--- soulution6/project1/obj/Release/net8.0/project1.AssemblyInfo.cs
+++ soulution6/project1/obj/Release/net8.0/project1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("project1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bb8b98e13a751f4989cf269e83daed06792e9f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("project1")]
