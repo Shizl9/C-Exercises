4780a6fd762b17e45620b6892f0136e8b6d3f449
Sheika <sheika02x2@gmail.com>
2026-04-21 15:04:55 +0400

    add task 1

---
+++ solution6/project1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter number of rows: ");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter number of columns: ");
+            //int cols = Convert.ToInt32(Console.ReadLine());
+
+            //int[,] numbers = new int[rows, cols];
+
+            //// إدخال القيم
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        Console.Write($"Enter value for [{i},{j}]: ");
+            //        numbers[i, j] = Convert.ToInt32(Console.ReadLine());
+            //    }
+            //}
+
+            //Console.WriteLine("\nArray values:");
+
+            //// طباعة القيم
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        Console.Write(numbers[i, j] + " ");
+            //    }
+            //    Console.WriteLine(); // سطر جديد بعد كل صف
+
+
+
+
+
+            //*******************Student Management System Using Arrays (C#) ********************
+
+
+
+        }
+    }
+    }
+}
